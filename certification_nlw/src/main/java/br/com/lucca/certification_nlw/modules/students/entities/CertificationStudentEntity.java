b51278d24d6edb19d855e5366f76f9bf6b34ad96
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-02-06 17:32:26 +0000

    feat: add Data (get & set) annotations e Constructor annotations

--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -1,8 +1,16 @@
 package br.com.lucca.certification_nlw.modules.students.entities;
 
 import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.List;
 
+@Data // Coloca automaticamente os Getters e Setters (MUITO UTIL BRIGADO LOMBOK)
+@AllArgsConstructor // Coloca os construtores automaticamente para todos os atributos
+@NoArgsConstructor  
 public class CertificationStudentEntity {
     
     private UUID id;
--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/StudentEntity.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/StudentEntity.java
@@ -3,6 +3,13 @@ package br.com.lucca.certification_nlw.modules.students.entities;
 import java.util.UUID;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Coloca automaticamente os Getters e Setters (MUITO UTIL BRIGADO LOMBOK)
+@AllArgsConstructor // Coloca os construtores automaticamente para todos os atributos
+@NoArgsConstructor  
 public class StudentEntity {
     
     private UUID id;
