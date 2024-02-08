be888df7db666a1789b4596b478ff7d99018d40e
lucas <lucaston2323@gmail.com>
2024-02-08 20:40:07 -0300

    fix code

--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/questions/entities/QuestionEntity.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/questions/entities/QuestionEntity.java
@@ -1,13 +1,17 @@
 package br.com.lucca.certification_nlw.modules.questions.entities;
 
+import java.util.List;
 import java.util.UUID;
 
 import org.hibernate.annotations.CreationTimestamp;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +26,15 @@ public class QuestionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(length = 50)
+    private String technology;
+
+    private String description;
+
+    @OneToMany
+    @JoinColumn(name = "question_id")
+    private List<AlternativeEntity> alternatives;
 
     @CreationTimestamp
     private LocalDateTime createdAt;
