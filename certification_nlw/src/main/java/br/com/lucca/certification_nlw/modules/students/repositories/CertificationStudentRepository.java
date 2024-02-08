4db5d9d7d9f30d7dfb5f8a0f8c6f0350ef2bf633
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-02-08 20:10:57 +0000

    feat repository & querys sql

---
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/repositories/CertificationStudentRepository.java
@@ -0,0 +1,18 @@
+package br.com.lucca.certification_nlw.modules.students.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+import java.util.List;
+
+import br.com.lucca.certification_nlw.modules.students.entities.CertificationStudentEntity;
+
+@Repository
+public interface CertificationStudentRepository extends JpaRepository<CertificationStudentEntity, UUID> {
+
+    @Query("SELECT c FROM certifications c INNER JOIN c.studentEntity std WHERE std.email = :email AND c.technology = :technology")
+    List<CertificationStudentEntity> findByStudentEmailAndTechnology(String email, String technology);
+
+}
--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/useCases/VerifyIfHasCertificationUseCase.java
@@ -1,14 +1,20 @@
 package br.com.lucca.certification_nlw.modules.students.useCases;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.lucca.certification_nlw.modules.students.dto.VerifyHasCertificationDTO;
+import br.com.lucca.certification_nlw.modules.students.repositories.CertificationStudentRepository;
 
 @Service // significa essa será a camada de serviço
 public class VerifyIfHasCertificationUseCase {
     
+    @Autowired
+    private CertificationStudentRepository certificationStudentRepository;
+
     public boolean execute( VerifyHasCertificationDTO dto ) {
-        if (dto.getEmail().equals("luaccminerva2323@outlook.com") && dto.getTechnology().equals("Java")) {
+        var result = this.certificationStudentRepository.findByStudentEmailAndTechnology(dto.getEmail(), dto.getTechnology());
+        if (!result.isEmpty()) {
             return true;
         } else {
             return false;
