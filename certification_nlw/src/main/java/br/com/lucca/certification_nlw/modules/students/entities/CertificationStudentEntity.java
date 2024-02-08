35cec5762636cd253d752e07df919c08b42e5a8f
lucas <lucaston2323@gmail.com>
2024-02-07 23:02:42 -0300

    fix: spring jpa bdd, feat: cardinalidades para as entidades e construção da tabela com o docker compose

--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/CertificationStudentEntity.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/CertificationStudentEntity.java
@@ -2,6 +2,13 @@ package br.com.lucca.certification_nlw.modules.students.entities;
 
 import java.util.UUID;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,12 +17,25 @@ import java.util.List;
 
 @Data // Coloca automaticamente os Getters e Setters (MUITO UTIL BRIGADO LOMBOK)
 @AllArgsConstructor // Coloca os construtores automaticamente para todos os atributos
-@NoArgsConstructor  
+@NoArgsConstructor
+@Entity(name = "Certifications")  // nome da entidade
 public class CertificationStudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    
+    @JoinColumn(name = "student_id")
     private UUID studentID;
+
+    @Column(length = 100)
     private String technology;
+
+    @Column(length = 10)
     private int grate;
-    List<AnswersCertificationsEntity> answersCertificationsEntity;
+
+    @ManyToOne
+    @JoinColumn(name = "student_id", insertable = false, updatable = false)
+    private StudentEntity studentEntity;
+    // List<AnswersCertificationsEntity> answersCertificationsEntity;
 }
--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/StudentEntity.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/students/entities/StudentEntity.java
@@ -3,6 +3,12 @@ package br.com.lucca.certification_nlw.modules.students.entities;
 import java.util.UUID;
 import java.util.List;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,9 +16,21 @@ import lombok.NoArgsConstructor;
 @Data // Coloca automaticamente os Getters e Setters (MUITO UTIL BRIGADO LOMBOK)
 @AllArgsConstructor // Coloca os construtores automaticamente para todos os atributos
 @NoArgsConstructor  
+@Entity (name = "students") // nome da entidade
 public class StudentEntity {
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+
+    @Column(unique = true, nullable = false, length = 100) // email é unico e not null
     private String email;
+
+    // OneToOne
+    // OneToMany que é o caso da entidade estudante ( um estudante para muitas ... )
+    // ManyToOne
+    // ManyToMany
+
+    @OneToMany(mappedBy = "studentEntity")
     private List<CertificationStudentEntity> certificationStudentEntity;
 }
