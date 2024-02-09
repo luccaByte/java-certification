64eed39ea8c452941f91ce8dc3ffca4e90b595e1
Lucas Gonçalves <103429796+Lucc-Minerva@users.noreply.github.com>
2024-02-09 17:58:34 +0000

    feat: add pathvariable annotation

--- certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/questions/controllers/QuestionController.java
+++ certification_nlw/src/main/java/br/com/lucca/certification_nlw/modules/questions/controllers/QuestionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import br.com.lucca.certification_nlw.modules.questions.entities.QuestionEntity;
 import br.com.lucca.certification_nlw.modules.questions.repositories.QuestionRepository;
@@ -18,7 +19,10 @@ public class QuestionController {
     private QuestionRepository questionRepository;
 
     @GetMapping("/technology/{technology}")
-    public List<QuestionEntity> findByTechnology (String technology) {
+    public List<QuestionEntity> findByTechnology (@PathVariable String technology) {
+
+        System.out.println("Tech: " + technology);
+
         return this.questionRepository.findByTechnology(technology);
     }
 }
